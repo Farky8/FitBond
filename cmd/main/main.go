7295e12f6be53b9117d31373900b9852c6fea865
Farky8 <pepefarky@gmail.com>
2025-01-21 18:27:12 +0100

    implemented main and setting up the router

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+    "net/http"
+
+    "github.com/Farky8/TrainMe/internal/api/storage"
+)
+
+func main() {
+    var trainings *storage.Trainings = storage.DBSetUp()
+    
+    var router http.Handler = api.SetRouter(trainings)
+    log.Fatal(http.ListenAndServe(":8080", router))
+}
--- internal/api/router.go
+++ internal/api/router.go
@@ -3,6 +3,7 @@ package api
 import (
     "net/http"
 
+    "github.com/urfave/neroni"
     "github.com/Farky8/TrainMe/internal/api/storage"
 )
 
@@ -11,4 +12,9 @@ func SetRouter(ts *storage.Trainings) http.Handler {
     router.HandleFunc("POST /home/create", func(w http.ResponseWriter, r *http.Request) { ts.HandlePostTraining(w, r) })
     router.HandleFunc("GET /home/search", func(w http.ResponseWriter, r *http.Request) { ts.HandleSearchTraining(w, r) })
     router.HandleFunc("POST /home/search/{id}", func(w http.ResponseWriter, r *http.Request) { ts.HandleGetTraining(w, r) })
+
+    n := negroni.Classic()
+    n.UseHandler(router)
+
+    return n
 }
