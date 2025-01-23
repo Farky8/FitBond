5fafb49d3cef617accf9624db9014befceedb9c6
Farky8 <pepefarky@gmail.com>
2025-01-23 22:18:56 +0100

    started js behind create

---
+++ frontEnd/script.js
@@ -0,0 +1,8 @@
+const form = document.querySelector('creation')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+
+    //TODO make a json and send it
+})
