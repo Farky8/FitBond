a7e44a941bcc86c6edeb14c0368bc9f43d953ed2
Farky8 <pepefarky@gmail.com>
2025-01-24 13:28:35 +0100

    wrote js for create, not tested

--- frontEnd/script.js
+++ frontEnd/script.js
@@ -1,8 +1,44 @@
-const form = document.querySelector('creation')
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const data = new FormData(event.target);
+function makeJson(requestdata) {
+    const requestObj = {
+        "heading": requestdata.get('heading'),
+        "about": requestdata.get('about'),
+        "capacity": requestdata.get('capacity'),
+        "price": requestdata.get('price')
+    }
 
-    //TODO make a json and send it
+    const requestJson = JSON.stringify(requestObj);
+    return requestJson;
+}
+
+async function sendJson(requestJson) {
+    const apiUrl = ".../home/create" // TODOO after gcp
+    try {
+        const response = await fetch(apiUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: requestJson,
+        })
+        if (response.status === 201) {
+            return 'Training successfully POSTED!';
+        } else {
+            return 'Invalid data given.';
+        }
+    } catch {
+        return 'Could not reach the server, try again later.'
+    }
+
+}
+
+const form = document.querySelector('#create-info')
+
+form.onsubmit('submit', event => {
+    event.preventDefault();  // the page will not refresh and redirect
+    const requestdata = new FormData(event.target);
+
+    const requestJson = makeJson(requestdata);
+
+    alert(sendJson(requestJson));
 })
